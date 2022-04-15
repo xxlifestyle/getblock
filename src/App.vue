94fdb5bd4f8453712e7767af9046ce9c5d9211da
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2022-04-15 08:22:34 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{date}}</div>
+</template>
+
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data(){
+    return{
+      date:1,
+    }
+  },
+  mounted() {
+
+  }
+};
+</script>
+
+
+<style>
+@import './assets/base.css';
+</style>
